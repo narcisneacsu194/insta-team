ee7a7585f6e1ae666151ea561b79e76f81250cfe
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-01-02 12:14:29 +0200

    Created a DAO interface and implementation for each model entity.

---
+++ src/main/java/com/teamtreehouse/instateam/dao/ProjectDaoImpl.java
@@ -0,0 +1,53 @@
+package com.teamtreehouse.instateam.dao;
+
+import com.teamtreehouse.instateam.model.Project;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProjectDaoImpl implements ProjectDao{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Project> findAll() {
+        Session session = sessionFactory.openSession();
+        Criteria criteria = session.createCriteria(Project.class);
+        List<Project> projects = criteria.list();
+        session.close();
+        return projects;
+    }
+
+    @Override
+    public Project findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Project project = session.get(Project.class, id);
+        session.close();
+        return project;
+    }
+
+    @Override
+    public void save(Project project) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(project);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void delete(Project project) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(project);
+        session.getTransaction().commit();
+        session.close();
+    }
+}
