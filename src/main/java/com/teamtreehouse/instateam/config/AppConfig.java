1a95e3fe4da01e0f61894fc10f3bb52126ffd30e
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-01-02 02:42:34 +0200

    Added a void main method in the Application class where the application is executed.
    Also created a configuration file for the Spring application.

---
+++ src/main/java/com/teamtreehouse/instateam/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.teamtreehouse.instateam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class AppConfig {
+
+}
